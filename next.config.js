4d20d78d7d520fe2ca90c43e49e0481249dba8c9
xhyrom <generalkubo@gmail.com>
2022-01-03 18:34:44 +0100

    secure headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+const { createSecureHeaders } = require('next-secure-headers');
+
 /** @type {import('next').NextConfig} */
 module.exports = {
 	reactStrictMode: true,
+	async headers() {
+		return [{
+			source: '/(.*)',
+			headers: createSecureHeaders({
+				forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
+				referrerPolicy: 'same-origin',
+			}),
+		}];
+	}
 };
